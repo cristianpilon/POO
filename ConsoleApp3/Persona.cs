59600989ffc9603d45f99d06e7c9385ea248f0f6
Cristian Pilon <cristianpilon@hotmail.com>
2022-05-23 00:21:00 -0300

    Agregada aplicación de pruebas.

---
+++ ConsoleApp3/Persona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class Persona
+    {
+        public int Edad { get; set; }
+
+        public string Nombre { get; set; }
+
+        public Persona()
+        {
+            Nombre = "Lautaro";
+            Edad = 20;
+        }
+
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        public string SaludoEnEspaniol() 
+        {
+            return ArmarSaludo("Hola");
+        }
+
+        public string SaludoEnIngles()
+        {
+            return ArmarSaludo("Hello");
+        }
+
+        private string ArmarSaludo(string saludo)
+        {
+            if (Nombre == "Amilcar")
+            {
+                return saludo + " guacheeen!!!";
+            }
+
+            return saludo + " " + Nombre;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Persona taro = new Persona();
+
+            Persona mary = new Persona("Matías", 29);
+            Persona amilcar = new Persona("Amilcar", 35);
+
+            Console.WriteLine(mary.SaludoEnEspaniol());
+            Console.WriteLine(taro.SaludoEnIngles());
+            Console.WriteLine(amilcar.SaludoEnEspaniol());
+        }
+    }
+}
